f4d2bc013f14247cc9487eb26647e8247e587000
unknown <202380059@psu.palawan.edu.ph>
2026-03-11 15:18:46 +0800

    Modifications in base, home, and manually add static files

--- hangarin/hangarin/settings.py
+++ hangarin/hangarin/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks',
-    'widget-tweaks',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- hangarin/tasks/views.py
+++ hangarin/tasks/views.py
@@ -6,3 +6,11 @@ class HomePageView(ListView):
     model = Task
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['total_tasks'] = Task.objects.count()
+        context['completed_tasks'] = Task.objects.filter(status='Completed').count()
+        context['inprogress_tasks'] = Task.objects.filter(status='In Progress').count()
+        context['pending_tasks'] = Task.objects.filter(status='Pending').count()
+        return context
